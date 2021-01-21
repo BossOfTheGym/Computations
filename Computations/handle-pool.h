b0dee3f86ed5b56cab547a6a59581498c3da76da
BossOfTheGym <vladimir.deikun1999@gmail.com>
2021-01-21 22:32:46 +0300

    handle utility

---
+++ Computations/handle-pool.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "handle.h"
+
+#include <vector>
+
+class HandlePool
+{
+public:
+	Handle acquire()
+	{
+		Handle newHandle{};
+		if (m_head == null)
+		{
+			newHandle = Handle{m_handles.size()};
+			m_handles.push_back(newHandle);
+		}
+		else
+		{
+			newHandle = m_head;
+			m_head = m_handles[m_head];
+			m_handles[newHandle] = newHandle;
+		}
+		return newHandle;
+	}
+
+	void release(Handle handle)
+	{
+		m_handles[handle] = m_head;
+		m_head = handle;
+	}
+
+	bool valid(Handle handle) const
+	{
+		return handle < m_handles.size() && m_handles[handle] == handle;
+	}
+
+
+private:
+	std::vector<Handle> m_handles;
+
+	Handle m_head;
+};
---
+++ Computations/handle.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "core.h"
+
+using Handle = u64;
+
+constexpr Handle null = std::numeric_limits<Handle>::max();
