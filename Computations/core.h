8c586c781e2d54b000423542741f5ec88f0e785c
BossOfTheGym <vladimir.deikun1999@gmail.com>
2021-01-21 22:31:58 +0300

    core

---
+++ Computations/core.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <filesystem>
+#include <cstdint>
+#include <cmath>
+
+namespace fs = std::filesystem;
+
+using i32 = std::int32_t;
+using u32 = std::uint32_t;
+using i64 = std::int64_t;
+using u64 = std::uint64_t;
+using f32 = float;
+using f64 = double;
