3d05453c5e5ee04ce4a6610aa6511ac1d3826e9d
BossOfTheGym <vladimir.deikun1999@gmail.com>
2021-01-22 18:02:12 +0300

    untested iterator

--- Computations/storage.h
+++ Computations/storage.h
@@ -7,7 +7,142 @@
 template<class Type>
 class Storage
 {
-	static_assert(std::is_move_assignable_v<Type> && std::is_default_constructible_v<Type>, "Type doesn't satisfy requirements.");
+	static_assert(std::is_move_assignable_v<Type> 
+		&& std::is_move_constructible_v<Type> 
+		&& std::is_default_constructible_v<Type>
+		, "Type doesn't satisfy requirements.");
+
+private:
+	// non-standart iterator
+	class Iterator
+	{
+		friend class Storage<Type>;
+
+		using Handles = std::vector<Handle>;
+		using Objects = std::vector<Type>;
+		using Index = i32;
+
+	private:
+		Iterator(const Handles& handles, Objects& objects, Index index) 
+			: m_handles{&handles}
+			, m_objects{objects}
+			, m_index{index}
+		{}
+
+	public:
+		Iterator() = default;
+
+	public:
+		auto operator -> ()
+		{
+			return std::tie((*m_handles)[m_index], (*m_objects)[m_index]);
+		}
+
+		auto operator * ()
+		{
+			return operator -> ();
+		}
+
+
+		Iterator& operator ++ ()
+		{
+			return ++m_index, *this;
+		}
+
+		Iterator operator ++ (int)
+		{
+			auto orig = *this;
+
+			return ++(*this), orig;
+		}
+
+		Iterator& operator -- ()
+		{
+			return --m_index, *this;
+		}
+
+		Iterator operator -- (int)
+		{
+			auto orig = *this;
+
+			return --(*this), orig;
+		}
+
+		Iterator& operator += (Index value)
+		{
+			m_index += value;
+			return *this;
+		}
+
+		Iterator operator + (Index value)
+		{
+			auto orig = *this;
+
+			return (orig += value);
+		}
+
+		Iterator& operator -= (Index value)
+		{
+			m_index -= value;
+			return *this;
+		}
+
+		Iterator operator - (Index value)
+		{
+			auto orig = *this;
+
+			return (orig -= value);
+		}
+
+		Index operator - (const Iterator& another)
+		{
+			return m_index - another.m_index;
+		}
+
+		auto operator [] (Index value)
+		{
+			Index i = m_index + value;
+
+			return std::tie((*m_handles)[i], (*m_objects)[i]);
+		}
+
+		bool operator == (const Iterator& another)
+		{
+			return m_index == another.m_index;
+		}
+
+		bool operator != (const Iterator& another)
+		{
+			return m_index != another.m_index;
+		}
+
+		bool operator < (const Iterator& another)
+		{
+			return m_index < another.m_index;
+		}
+
+		bool operator > (const Iterator& another)
+		{
+			return m_index > another.m_index;
+		}
+
+		bool operator <= (const Iterator& another)
+		{
+			return m_index <= another.m_index;
+		}
+
+		bool operator >= (const Iterator& another)
+		{
+			return m_index >= another.m_index;
+		}
+
+
+	private:
+		const Handles* m_handles{nullptr};
+		Objects* m_objects{nullptr};
+		i32 m_index{-1};
+	};
+
 
 private:
 	void assure(Handle handle)
@@ -23,10 +158,11 @@ private:
 		}
 	}
 
+
 public:
 	void add(Handle handle)
 	{
-		assert(handle != null);
+		assert(handle != null && !has(handle));
 
 		assure(handle);
 		if (m_sparse[handle] == null)
@@ -92,24 +228,40 @@ public:
 	// TODO : iterator
 	auto begin()
 	{
-		return m_objects.begin();
+		return m_packed.begin();
 	}
 
 	auto begin() const
 	{
-		return m_objects.begin();
+		return m_packed.begin();
 	}
 
 	auto end()
 	{
-		return m_objects.end();
+		return m_packed.end();
 	}
 
 	auto end() const
 	{
-		return m_objects.end();
+		return m_packed.end();
 	}
 
+
+	i32 size() const
+	{
+		return m_packed.size();
+	}
+
+
+	Iterator pairsBegin()
+	{
+		return Iterator{m_packed, m_objects, 0};
+	}
+
+	Iterator pairsEnd()
+	{
+		return Iterator{m_packed, m_objects, typename Iterator::Index{m_packed.size()}};
+	}
 
 private:
 	std::vector<Handle> m_sparse;
