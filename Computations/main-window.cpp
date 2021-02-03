cc18d9c8c7d502c39dcedc1c14b2c4e124db3589
BossOfTheGym <vladimir.deikun1999@gmail.com>
2021-02-03 18:49:24 +0300

    added signals to main-window

--- Computations/main-window.cpp
+++ Computations/main-window.cpp
@@ -1,6 +1,169 @@
 #include "main-window.h"
 
+namespace win
+{
+	MainWindow::MainWindow(const glfw::CreationInfo& info) : glfw::Window(info)
+	{}
 
-MainWindow::MainWindow(const glfw::CreationInfo& info) 
-	: glfw::Window(info)
-{}
+	// sinks(public)
+	auto MainWindow::keyPressSink() -> KeyPressSink
+	{
+		return KeyPressSink{m_keyPressSigh};
+	}
+
+	auto MainWindow::charSink() -> CharSink
+	{
+		return CharSink{m_charSigh};
+	}
+
+	auto MainWindow::mouseMoveSink() -> MouseMoveSink
+	{
+		return MouseMoveSink{m_mouseMoveSigh};
+	}
+
+	auto MainWindow::mouseButtonSink() -> MouseButtonSink
+	{
+		return MouseButtonSink{m_mouseButtonSigh};
+	}
+
+	auto MainWindow::mouseEnterSink() -> MouseEnterSink
+	{
+		return MouseEnterSink{m_mouseEnterSigh};
+	}
+
+	auto MainWindow::scrollSink() -> ScrollSink
+	{
+		return ScrollSink{m_scrollSigh};
+	}
+
+	auto MainWindow::dropSink() -> DropSink
+	{
+		return DropSink{m_dropSigh};
+	}
+
+	auto MainWindow::closeSink() -> CloseSink
+	{
+		return CloseSink{m_closeSigh};
+	}
+
+	auto MainWindow::resizeSink() -> ResizeSink 
+	{
+		return ResizeSink{m_resizeSigh};
+	}
+
+	auto MainWindow::framebufferResizeSink() -> FramebufferResizeSink
+	{
+		return FramebufferResizeSink{m_framebufferResizeSigh};
+	}
+
+	auto MainWindow::moveSink() -> MoveSink
+	{
+		return MoveSink{m_moveSigh};
+	}
+
+	auto MainWindow::minimizeSink() -> MinimizeSink
+	{
+		return MinimizeSink{m_minimizeSigh};
+	}
+
+	auto MainWindow::maximizeSink() -> MaximizeSink
+	{
+		return MaximizeSink{m_maximizeSigh};
+	}
+
+	auto MainWindow::focusSink() -> FocusSink
+	{
+		return FocusSink{m_focusSigh};
+	}
+
+	auto MainWindow::refreshSink() -> RefreshSink
+	{
+		return RefreshSink{m_refreshSigh};
+	}
+
+	auto MainWindow::contentScaleSink() -> ContentScaleSink 
+	{
+		return ContentScaleSink{m_contentScaleSigh};
+	}
+
+	// overriden(private)
+	void MainWindow::keyPressEvent(int key, int scancode, int action, int mods)
+	{
+		m_keyPressSigh.publish(key, scancode, action, mods);
+	}
+
+	void MainWindow::charEvent(unsigned int codepoint)
+	{
+		m_charSigh.publish(codepoint);
+	}
+
+	void MainWindow::mouseMoveEvent(double xpos, double ypos)
+	{
+		m_mouseMoveSigh.publish(xpos, ypos);
+	}
+
+	void MainWindow::mouseButtonEvent(int button, int action, int mods)
+	{
+		m_mouseButtonSigh.publish(button, action, mods);
+	}
+
+	void MainWindow::mouseEnterEvent(int entered)
+	{
+		m_mouseEnterSigh.publish(entered);
+	}
+
+	void MainWindow::scrollEvent(double xOffset, double yOffset)
+	{
+		m_scrollSigh.publish(xOffset, yOffset);
+	}
+
+	void MainWindow::dropEvent(int pathCount, const char* paths[])
+	{
+		m_dropSigh.publish(pathCount, paths);
+	}
+
+	void MainWindow::closeEvent()
+	{
+		m_closeSigh.publish();
+	}
+
+	void MainWindow::resizeEvent(int width, int height)
+	{
+		m_resizeSigh.publish(width, height);
+	}
+
+	void MainWindow::framebufferResizeEvent(int width, int height)
+	{
+		m_framebufferResizeSigh.publish(width, height);
+	}
+
+	void MainWindow::moveEvent(int xPos, int yPos)
+	{
+		m_moveSigh.publish(xPos, yPos);
+	}
+
+	void MainWindow::minimizeEvent(int minimized)
+	{
+		m_minimizeSigh.publish(minimized);
+	}
+
+	void MainWindow::maximizeEvent(int maximized)
+	{
+		m_maximizeSigh.publish(maximized);
+	}
+
+	void MainWindow::focusEvent(int focused)
+	{
+		m_focusSigh.publish(focused);
+	}
+
+	void MainWindow::refreshEvent()
+	{
+		m_refreshSigh.publish();
+	}
+
+	void MainWindow::contentScaleEvent(float xScale, float yScale)
+	{
+		m_contentScaleSigh.publish(xScale, yScale);
+	}
+}
--- Computations/main-window.h
+++ Computations/main-window.h
@@ -1,13 +1,138 @@
 #pragma once
 
 #include <glfw-cxx/glfw3.h>
+#include <entt/signal/delegate.hpp>
+#include <entt/signal/sigh.hpp>
 
-class MainWindow : public glfw::Window
+namespace win
 {
-public:
-	MainWindow(const glfw::CreationInfo& info = {});
+	class MainWindow : public glfw::Window
+	{
+	public:
+		using KeyPressSigh          = entt::sigh<void(int, int, int, int)>;
+		using CharSigh              = entt::sigh<void(unsigned int)>;
+		using MouseMoveSigh         = entt::sigh<void(double, double)>;
+		using MouseButtonSigh       = entt::sigh<void(int, int, int)>;
+		using MouseEnterSigh        = entt::sigh<void(int)>;
+		using ScrollSigh            = entt::sigh<void(double, double)>;
+		using DropSigh              = entt::sigh<void(int, const char*[])>;
+		using CloseSigh             = entt::sigh<void()>;
+		using ResizeSigh            = entt::sigh<void(int, int)>;
+		using FramebufferResizeSigh = entt::sigh<void(int, int)>;
+		using MoveSigh              = entt::sigh<void(int, int)>;
+		using MinimizeSigh          = entt::sigh<void(int)>;
+		using MaximizeSigh          = entt::sigh<void(int)>;
+		using FocusSigh             = entt::sigh<void(int)>;
+		using RefreshSigh           = entt::sigh<void()>;
+		using ContentScaleSigh      = entt::sigh<void(float, float)>;
+
+		using KeyPressSink          = KeyPressSigh          ::sink_type;
+		using CharSink              = CharSigh              ::sink_type;
+		using MouseMoveSink         = MouseMoveSigh         ::sink_type;
+		using MouseButtonSink       = MouseButtonSigh       ::sink_type;
+		using MouseEnterSink        = MouseEnterSigh        ::sink_type;
+		using ScrollSink            = ScrollSigh            ::sink_type;
+		using DropSink              = DropSigh              ::sink_type;
+		using CloseSink             = CloseSigh             ::sink_type;
+		using ResizeSink            = ResizeSigh            ::sink_type;
+		using FramebufferResizeSink = FramebufferResizeSigh ::sink_type;
+		using MoveSink              = MoveSigh              ::sink_type;
+		using MinimizeSink          = MinimizeSigh          ::sink_type;
+		using MaximizeSink          = MaximizeSigh          ::sink_type;
+		using FocusSink             = FocusSigh             ::sink_type;
+		using RefreshSink           = RefreshSigh           ::sink_type;
+		using ContentScaleSink      = ContentScaleSigh      ::sink_type;
 
 
-public:
+	public:
+		MainWindow(const glfw::CreationInfo& info = {});
 
-};
+
+	public: // sink methods, all return objects that are used to connect handlers
+		KeyPressSink keyPressSink();
+
+		CharSink charSink();
+
+		MouseMoveSink mouseMoveSink();
+
+		MouseButtonSink mouseButtonSink();
+
+		MouseEnterSink mouseEnterSink();
+
+		ScrollSink scrollSink();
+
+		DropSink dropSink();
+
+		CloseSink closeSink();
+
+		ResizeSink resizeSink();
+
+		FramebufferResizeSink framebufferResizeSink();
+
+		MoveSink moveSink();
+
+		MinimizeSink minimizeSink();
+
+		MaximizeSink maximizeSink();
+
+		FocusSink focusSink();
+
+		RefreshSink refreshSink();
+
+		ContentScaleSink contentScaleSink();
+
+
+	private: // redirects events to signals 
+		virtual void keyPressEvent(int key, int scancode, int action, int mods) override;
+
+		virtual void charEvent(unsigned int codepoint) override;
+
+		virtual void mouseMoveEvent(double xpos, double ypos) override;
+
+		virtual void mouseButtonEvent(int button, int action, int mods) override;
+
+		virtual void mouseEnterEvent(int entered) override;
+
+		virtual void scrollEvent(double xOffset, double yOffset) override;
+
+		virtual void dropEvent(int pathCount, const char* paths[]) override;
+
+
+		virtual void closeEvent() override;
+
+		virtual void resizeEvent(int width, int height) override;
+
+		virtual void framebufferResizeEvent(int width, int height) override;
+
+		virtual void moveEvent(int xPos, int yPos) override;
+
+		virtual void minimizeEvent(int minimized) override;
+
+		virtual void maximizeEvent(int maximized) override;
+
+		virtual void focusEvent(int focused) override;
+
+		virtual void refreshEvent() override;
+
+		virtual void contentScaleEvent(float xScale, float yScale) override;
+
+
+	private:
+		KeyPressSigh          m_keyPressSigh;
+		CharSigh              m_charSigh;
+		MouseMoveSigh         m_mouseMoveSigh;
+		MouseButtonSigh       m_mouseButtonSigh;
+		MouseEnterSigh        m_mouseEnterSigh;
+		ScrollSigh            m_scrollSigh;
+		DropSigh              m_dropSigh;
+		CloseSigh             m_closeSigh;
+		ResizeSigh            m_resizeSigh;
+		FramebufferResizeSigh m_framebufferResizeSigh;
+		MoveSigh              m_moveSigh;
+		MinimizeSigh          m_minimizeSigh;
+		MaximizeSigh          m_maximizeSigh;
+		FocusSigh             m_focusSigh;
+		RefreshSigh           m_refreshSigh;
+		ContentScaleSigh      m_contentScaleSigh;
+	};	
+}
