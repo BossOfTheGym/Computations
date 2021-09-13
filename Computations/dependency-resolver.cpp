dab02565cc1e3a045a5fbbdb942f9944530ef0e5
BossOfTheGym <vladimir.deikun1999@gmail.com>
2021-09-13 21:03:46 +0300

    dependency resolver

---
+++ Computations/dependency-resolver.cpp
@@ -0,0 +1,63 @@
+#include "dependency-resolver.h"
+
+void DependencyResolver::clear()
+{
+	m_storage.clear();
+	m_order.clear();
+}
+
+bool DependencyResolver::addDependencies(const std::string& name, const Dependencies& dependencies)
+{
+	auto [it, inserted] = m_storage.insert({name, Label::White, dependencies});
+	return inserted;
+}
+
+bool DependencyResolver::removeDependencies(const std::string& name)
+{
+	return m_storage.erase(name) != 0;
+}
+
+bool DependencyResolver::resolve(Dependencies& deps)
+{
+	m_order.clear();
+	m_curr = m_storage.size();
+
+	m_order.resize(m_curr);
+	for (auto it = m_storage.begin(), e = m_storage.end(); i != e; i++) {
+		auto& [name, entry] = *it;
+		if (entry.label == Labels::White && !dfs(it)) {
+			return false;
+		}
+	}
+
+	deps = std::move(m_order);
+	m_storage.clear();
+
+	return true;
+}
+
+bool DependencyResolver::dfs(Iterator it)
+{
+	if (it == m_storage.end()) { // dangling vertex found, error
+		return false;
+	}
+	
+	auto& [name, entry] = it->second;
+	if (entry.label == Label::Gray) { // cycle detected, cannot build dependency order
+		return false;
+	}
+	if (entry.label == Label::Black) { // already visited, return success
+		return true;
+	}
+
+	entry.label = Label::Gray;
+	for (auto& dep : entry.dependencies) {
+		auto next = m_storage.find(dep);
+		if (!dfs(next)) {
+			return false;
+		}
+	}
+	entry.label = Label::Black;
+
+	m_order[--m_curr] = name;
+}
--- Computations/dependency-resolver.h
+++ Computations/dependency-resolver.h
@@ -1,9 +1,43 @@
 #pragma once
 
+#include <unordered_map>
+
+#include <core.h>
+
 #include "dependency.h"
 
 class DependencyResolver
 {
 public:
-	std::vector<Dependency*> resolve();
+	enum class Label
+	{
+		White,
+		Gray,
+		Black
+	};
+
+	struct Entry
+	{
+		Label label{};
+		Dependencies dependencies;
+	};
+
+	using Storage  = std::unordered_map<std::string, Entry>;
+	using Iterator = typename Storage::iterator;
+
+public:
+	void clear();
+
+	bool addDependencies(const std::string& name, const Dependencies& dependencies);
+	bool removeDependencies(const std::string& name);
+
+	bool resolve(Dependencies& deps);
+
+private:
+	bool dfs(Iterator it);
+
+private:
+	Storage      m_storage;
+	Dependencies m_order;
+	uint m_curr{};
 };
