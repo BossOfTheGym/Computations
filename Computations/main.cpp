9c302b8272a8155d04800f1e18c6838ab8e17a26
BossOfTheGym <vladimir.deikun1999@gmail.com>
2022-02-26 13:45:02 +0300

    chaotic test

--- Computations/main.cpp
+++ Computations/main.cpp
@@ -15,11 +15,11 @@ void test_tiled()
 	builder.setWindowWidth(1536);
 	builder.setWindowHeight(512);
 	builder.setTotalUpdates(1000);
-	for (auto split : {63, 127, 255, 511, 1023, 2047}) {
-		for (auto step : {3, 4, 5, 6}) {
+	for (auto split : {127, 255, 511, 1023}) {
+		for (auto step : {3, 4, 5}) {
 			for (auto work : {8, 16, 32}) {
 				std::ostringstream output;
-				output << "tests/tiled_" << split << "_" << step << "_" << work << ".json";
+				output << "tests/tiled/test_" << split << "_" << step << "_" << work << ".json";
 
 				builder.setSteps(step);
 				builder.setSplitX(split);
@@ -44,10 +44,10 @@ void test_simple()
 	builder.setWindowWidth(512);
 	builder.setWindowHeight(512);
 	builder.setTotalUpdates(1000);
-	for (auto split : {63, 127, 255, 511, 1023, 2047}) {
+	for (auto split : {127, 255, 511, 1023}) {
 		for (auto work : {8, 16, 32}) {
 			std::ostringstream output;
-			output << "tests/simple_" << split << "_" << work<< ".json";
+			output << "tests/simple/test_" << split << "_" << work<< ".json";
 
 			builder.setSplitX(split);
 			builder.setSplitY(split);
@@ -64,18 +64,18 @@ void test_simple()
 void custom_test()
 {
 	ConfigBuilder builder;
-	builder.setSystems({"red_black_tiled", "red_black_smtm"});
+	builder.setSystems({"jacoby", "chaotic_tiled", "chaotic_smtm"});
 	builder.setOutput("test/output.json");
-	builder.setSplitX(2047);
-	builder.setSplitY(2047);
-	builder.setGridX(2);
+	builder.setSplitX(1023);
+	builder.setSplitY(1023);
+	builder.setGridX(3);
 	builder.setGridY(1);
-	builder.setWindowWidth(1024);
+	builder.setWindowWidth(1536);
 	builder.setWindowHeight(512);
-	builder.setTotalUpdates(1000);
+	builder.setTotalUpdates(100000);
 	builder.setSteps(5);
-	builder.setWorkgroupSizeX(32);
-	builder.setWorkgroupSizeY(32);
+	builder.setWorkgroupSizeX(8);
+	builder.setWorkgroupSizeY(8);
 
 	auto application = std::make_unique<app::App>(builder.build());
 	application->mainloop();
@@ -83,8 +83,8 @@ void custom_test()
 
 int main()
 {
-	test_tiled();
-	test_simple();	
-
+	//test_tiled();
+	//test_simple();	
+	custom_test();
 	return 0;
 }
